4c0b25fee10e94ccfa9124c8193044b274b6413d
Joyal George K J <joyalgeorgekj@gmail.com>
2026-03-31 00:47:40 +0530

    Recipe page design done

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -3,6 +3,10 @@ import { loadMarkdown } from "@/lib/loader";
 import { formatMarkdown } from "@/lib/parser";
 import raw from "../../atlas.json";
 import { Atlas } from "@/types/atlas";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Link from "next/link";
+import Breadcrumps from "@/components/Breadcrumps";
 
 const converter = new MarkdownIt({
     html: false,
@@ -24,8 +28,23 @@ export default async function Page({
     const data = converter.render(content);
 
     return (
+        <div className="min-h-screen font-sans antialiased text-[#141414] min-w-275 max-w-275 mx-auto">
+            {/* Header */}
             <Header />
 
+            {/* <!-- Breadcrumb --> */}
+            <Breadcrumps
+                navigations={{
+                    library: "/library",
+                    [metadata.type]: "/" + metadata.type,
+                    [metadata.name]: "/" + metadata.name,
+                }}
+            />
+
+            {/* Main Section */}
+            <main className="prose py-4 flex flex-col gap-4">
+                <article dangerouslySetInnerHTML={{ __html: data }}></article>
+            </main>
 
             {/* Footer */}
             <Footer />
