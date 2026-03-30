c4854b84d9a90470256ac1cb9004b828054ac047
Joyal George K J <joyalgeorgekj@gmail.com>
2026-03-31 00:47:24 +0530

    Landing page design done.

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,33 @@
 import Link from "next/link";
 import raw from "../atlas.json";
+import Image from "next/image";
 import { Atlas } from "@/types/atlas";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+type typeType = {
+    [key: string]: string;
+};
 
 export default async function Home() {
     const search = raw as Atlas;
 
+    const typeIcons: typeType = {
+        metal: "/pickaxe.png",
+    };
+
     return (
-        <div className="mx-auto w-4xl">
+        <div className="min-h-screen font-sans antialiased text-[#141414] max-w-275 mx-auto px-6">
             {/* Header */}
             <Header />
 
             {/* Main Section */}
-            <main className="flex flex-col text-center items-center gap-4 min-h-screen">
+            <main className="py-10 text-center flex flex-col gap-4">
                 {/* Introduction */}
-                <h3 className="text-5xl font-medium leading-[1.2] mt-12">Rebuild Civilization from First Priciples</h3>
-                <p className="text-[20px] font-light leading-1.5">
+                <h3 className="text-5xl font-bold tracking-tight text-[#141414]">
+                    Rebuild Civilization from First Priciples
+                </h3>
+                <p className="text-lg text-gray-500 max-w-2xl mx-auto">
                     A comrephensive survival knowledge library structured for
                     clarity and usablity.
                 </p>
@@ -28,15 +41,32 @@ export default async function Home() {
                     name="main-search"
                     id="main-search"
                     placeholder="Search"
-                    className="border-2 border-neutral-500/25 w-3/5 rounded-md p-1 px-4 shadow"
+                    className="relative max-w-162.5 mx-auto w-full h-14 pl-12 pr-4 rounded-xl border border-gray-200 bg-white shadow-sm text-base focus:shadow-md transition-shadow outline-none"
                 />
 
                 {/* Items */}
-                {Object.keys(search.nodes).map((items, index) => (
-                    <Link href={"/" + items} key={index}>
-                        {search.nodes[items].title}
-                    </Link>
-                ))}
+                <div className="h-[42dvh] overflow-y-scroll p-2 shadow-lg border border-neutral-500/25 rounded-md items">
+                    {Object.keys(search.nodes).map((items, index) => (
+                        <Link
+                            href={"/" + items}
+                            key={index}
+                            className="flex items-center gap-4 px-6 py-4 border-b border-gray-500/25 hover:bg-[#F4FBF6] transition-colors cursor-pointer group"
+                        >
+                            <Image
+                                src={typeIcons[search.nodes[items].type]}
+                                alt="icon"
+                                width={32}
+                                height={32}
+                            />
+                            <span className="uppercase tracking-wider text-[#28ED58]">
+                                [{search.nodes[items].type}]:
+                            </span>
+                            <span className="text-[15px] font-medium text-[#141414]">
+                                {search.nodes[items].title}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
             </main>
             {/* Footer */}
             <Footer />
