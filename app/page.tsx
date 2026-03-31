1f8c2146ab951fcc991a156b2eca157295202eb5
Joyal George K J <joyalgeorgekj@gmail.com>
2026-03-31 19:14:08 +0530

    Search function added not implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import raw from "../atlas.json";
 import Image from "next/image";
 import { Atlas } from "@/types/atlas";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { searchNodes } from "@/lib/search";
 
 type typeType = {
     [key: string]: string;
 };
 
-export default async function Home() {
+export default function Home() {
     const search = raw as Atlas;
 
     const typeIcons: typeType = {
@@ -41,6 +44,11 @@ export default async function Home() {
                     name="main-search"
                     id="main-search"
                     placeholder="Search"
+                    onChange={(e) =>
+                        console.log(
+                            searchNodes(e.currentTarget.value, search.nodes),
+                        )
+                    }
                     className="relative max-w-162.5 mx-auto w-full h-14 pl-12 pr-4 rounded-xl border border-gray-200 bg-white shadow-sm text-base focus:shadow-md transition-shadow outline-none"
                 />
 
