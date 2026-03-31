88027a22e549751a7f4c5ca346cf18b245a97fd3
Joyal George K J <joyalgeorgekj@gmail.com>
2026-03-31 19:13:19 +0530

    basic search function implemented

---
+++ lib/search.ts
@@ -0,0 +1,21 @@
+import { Atlas, AtlasNode } from "@/types/atlas";
+
+export function searchNodes(query: string, nodes: Atlas["nodes"]) {
+    const q = query.toLowerCase();
+
+    let result: Record<string, AtlasNode> = {};
+
+    const rawResult = Object.entries(nodes).filter(([key, value]) => {
+        return (
+            key.includes(q) ||
+            value.keywords.includes(q) ||
+            value.title.toLowerCase() === q
+        );
+    });
+
+    rawResult.forEach(raw => {
+        result[raw[0]] = raw[1]
+    });
+
+    return result;
+}
