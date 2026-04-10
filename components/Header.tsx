e8e41b8ef4d25d708e62b3e14fd26e60f160f120
Joyal George K J <joyalgeorgekj@gmail.com>
2026-04-10 20:58:45 +0530

    Removed search from header to remove complexity and improve the engine

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "@/app/assets/images/logo.png"
+import Logo from "@/app/assets/images/logo.png";
 
 function Header() {
     return (
@@ -9,8 +9,17 @@ function Header() {
             <header className="">
                 {/* Nav bar */}
                 <nav className="h-20 flex items-center justify-between border-b border-gray-100">
-                    <Link href={"/"} className="flex gap-2 items-center outline-0">
-                        <Image src={Logo} alt="" width={32} height={32} className="rounded-md" />
+                    <Link
+                        href={"/"}
+                        className="flex gap-2 items-center outline-0"
+                    >
+                        <Image
+                            src={Logo}
+                            alt=""
+                            width={32}
+                            height={32}
+                            className="rounded-md"
+                        />
                         <h2 className="text-xl font-bold tracking-tight text-[#141414]">
                             Senku <span className="text-[#28ED58]">IO</span>
                         </h2>
@@ -25,18 +34,6 @@ function Header() {
                         </li>
                         <li className="flex gap-8 text-sm font-medium text-gray-600 hover:text-[#141414] transition-colors">
                             <a href="">About</a>
-                        </li>
-                        <li>
-                            <label htmlFor="search" className="sr-only">
-                                Search recipie
-                            </label>
-                            <input
-                                type="text"
-                                name="search"
-                                id="search"
-                                placeholder="Search survival knowledge"
-                                className="w-60 h-9 px-4 rounded-full bg-white border border-gray-200 text-xs focus:ring-1 focus:ring-[#28ED58] outline-none"
-                            />
                         </li>
                     </ul>
                 </nav>
