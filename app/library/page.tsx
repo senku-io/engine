5f427ea8250392bc2ec0d1f9f1d1c92b7b23957f
Joyal George K J <joyalgeorgekj@gmail.com>
2026-04-13 22:13:01 +0530

    Basic library page created

---
+++ app/library/page.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import raw from "../../atlas.json";
+import Image from "next/image";
+import { Atlas } from "@/types/atlas";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { searchNodes } from "@/lib/search";
+import { IconMap } from "@/constants/iconMap";
+
+export default function Library() {
+    const search = (raw as Atlas).nodes;
+
+    const categories = ["material", "food"];
+
+    return (
+        <div className="min-h-screen font-sans antialiased text-[#141414] max-w-275 w-275 mx-auto px-6">
+            {/* Header */}
+            <Header />
+
+            {/* Main Section */}
+            <main className="py-10 text-center flex flex-col gap-8">
+                {categories.map((val, ind) => (
+                    <div className="items" key={ind}>
+                        <h2 className="text-start capitalize font-bold text-xl pb-4">
+                            {val.replaceAll("_", " ")}
+                        </h2>
+                        <div className="flex gap-4">
+                            {Object.keys(
+                                searchNodes(val, search, "type", 10),
+                            ).map((items, index) => (
+                                <Link
+                                    href={`/library/${search[items].type}/${items}`}
+                                    key={index}
+                                    className="w-fit flex items-center gap-4 px-6 py-4 border border-gray-500/25 shadow shadow-gray-500/25 hover:bg-[#F4FBF6] transition-colors cursor-pointer group"
+                                >
+                                    <Image
+                                        src={IconMap[search[items].type]}
+                                        alt="icon"
+                                        width={32}
+                                        height={32}
+                                    />
+                                    <span className="text-[15px] font-medium text-[#141414]">
+                                        {search[items].title}
+                                    </span>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </main>
+            {/* Footer */}
+            <Footer />
+        </div>
+    );
+}
