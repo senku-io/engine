b19175623db7695d7191e1e79a6626bea1a335a5
Joyal George K J <joyalgeorgekj@gmail.com>
2026-04-16 20:44:24 +0530

    Contribute support page

---
+++ app/writer/page.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function Writer() {
+    return (
+        <div className="min-h-screen font-sans antialiased text-[#141414] max-w-275 w-275 mx-auto px-6">
+            {/* Header */}
+            <Header />
+
+            {/* Main Section */}
+            <main className="py-10 text-center flex flex-col gap-4">
+                
+            </main>
+            {/* Footer */}
+            <Footer />
+        </div>
+    );
+}
